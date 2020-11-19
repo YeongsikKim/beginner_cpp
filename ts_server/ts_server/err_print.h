c5e756944364cb31cb9ca222399b03b78733af11
YeongsikKim <byeolhakim42@gmail.com>
2020-11-19 10:09:43 +0900

    ts_server resend

--- ts_server/ts_server/err_print.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-//Print Error
-VOID err_quit(char *msg);
-VOID err_display(char *msg);
-VOID err_display(int errcode);
--- ts_server/ts_server/tetris_server.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-
-//About Windows Message
-LRESULT CALLBACK WndProc(HWND, UINT,WPARAM, LPARAM);
-VOID ProcessSocketMessage(HWND, UINT, WPARAM, LPARAM);
-
